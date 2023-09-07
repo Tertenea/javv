f90fdb18f568f80aa4e6e72bd08eb8ec9ae75ff1
Tert <56269917+Tertenea@users.noreply.github.com>
2023-09-07 13:30:29 +0300

    Update h4.java

--- h4.java
+++ h4.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class h4 {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Sisesta mingi tekst: ");
         String tekst = scanner.nextLine();
         System.out.println("Sisestatud tekst: " + tekst);
