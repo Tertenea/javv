45b95d7ce8bdfa905b720b441aca491c74a08e2a
Tert <56269917+Tertenea@users.noreply.github.com>
2023-09-07 13:17:07 +0300

    Update h3.java

--- h3.java
+++ h3.java
@@ -3,21 +3,21 @@ public class h3 {
         int[] massiiv = { 12, 5, 10, 10, 28, 17, 5, 19, 0, 13, 0, 22, 7, 0, 17, 2, 24, 1, 13, 29, 0, 7, 16, 8, 7, 4, 27,
                 8, 8, 23, 26, 25, 15, 2, 26, 1 };
 
-        System.out.println("Esimene element: " + massiiv[0]);
+        System.out.println("essa on: " + massiiv[0]);
 
-        System.out.println("Viimane element: " + massiiv[massiiv.length - 1]);
+        System.out.println("viimane on: " + massiiv[massiiv.length - 1]);
 
-        int elementideArv = massiiv.length;
-        System.out.println("Elementide arv massiivis: " + elementideArv);
+        int numbridKokku = massiiv.length;
+        System.out.println("kokku numbreid: " + numbridKokku);
 
         int summa = 0;
-        for (int i = 0; i < elementideArv; i++) {
+        for (int i = 0; i < numbridKokku; i++) {
             summa += massiiv[i];
         }
-        System.out.println("Elementide summa massiivis: " + summa);
+        System.out.println("numbrite summa: " + summa);
 
-        double keskmine = (double) summa / elementideArv;
-        System.out.println("Aritmeetiline keskmine: " + keskmine);
+        double keskmine = (double) summa / numbridKokku;
+        System.out.println("massiivi keskmine: " + keskmine);
 
         int[][] massiiv2 = { { 1, 23 }, { 2, 15 }, { 3, 29 }, { 4, 15 }, { 5, 26 }, { 6, 17 }, { 7, 26 }, { 8, 15 },
                 { 9, 28 }, { 10, 12 }, { 11, 24 }, { 12, 16 }, { 13, 21 }, { 14, 10 }, { 15, 10 }, { 16, 26 },
